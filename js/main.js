b3d7270051e50b7b375cee4fff163c3ff4ef43a9
Moon-YJ <tunjeong@gmail.com>
2023-11-17 13:14:57 +0900

    Youtube 데이터 호출 및 동적 DOM 생성

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const api_key = 'AIzaSyB81cXmxoWdzbYs8QZUlN_LQskZFT_Xqoo';
 const playlist_id = 'PLMaY0ixOiyljR7EsFnCk9HPiR7eNsI6Yd';
 const base_url = 'https://www.googleapis.com/youtube/v3/playlistItems';
-const result_url = `${base_url}?part=snippet&api_key=${api_key}&playlistId=${playlist_id}&maxResults=5`;
+const num = 7;
+const result_url = `${base_url}?part=snippet&key=${api_key}&playlistId=${playlist_id}&maxResults=${num}`;
 
 /*
   GET 방식
@@ -9,3 +10,20 @@ const result_url = `${base_url}?part=snippet&api_key=${api_key}&playlistId=${pla
     - 요청 URL: 기본URL?name=value&name=value
     - QueryString: 기본 요청 URL ?물음표 뒤로 name=value 세트로 옵션을 담아서 전달하는 방식
 */
+
+fetch(result_url)
+	.then((data) => data.json())
+	.then((json) => {
+		console.log(json.items);
+		let tags = '';
+		json.items.forEach((el) => {
+			tags += `
+        <article>
+          <img src=${el.snippet.thumbnails.standard.url}/>
+          <h2>${el.snippet.title}<h2/>
+          <p>${el.snippet.description}</p>
+        </article>
+      `;
+		});
+		document.body.innerHTML = tags;
+	});
